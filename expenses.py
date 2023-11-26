340d9b9ff215b105359debd57bbbf6bbd0ced1e1
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-11-25 21:36:13 -0600

    changed loop into sum fuction

--- expenses.py
+++ expenses.py
@@ -1,8 +1,7 @@
 expenses = [10.50, 8, 5, 15, 20, 5, 3];
 
-sum = 0;
+total = sum(expenses)
 
 
-for x in expenses:
-    sum = sum + x;
-print('You spent $',sum, sep='');
+
+print('You spent $',total, sep='');
