3e7694401f0c259e6e755c5bd8f9ff7a08f01362
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-11-25 19:19:46 -0600

    weather if else app

---
+++ weather.py
@@ -0,0 +1,27 @@
+temperature = 75;
+forecast ="sunny"
+
+# if temperature > 80:
+#     print("It's too hot!")
+#     print("Stay inside!");
+# elif temperature <60:
+#     print("It's too cold!")
+#     print("Stay inside!")
+# else: print("Have a nice day!");
+
+# if temperature < 80 and forecast != "rainy":
+#     print("Go outside!");
+# else:
+#     print("Stay inside!")
+if not forecast == "rainy":
+    print("Go outside!");
+else:
+    print("Stay inside!");
+raining = True;
+if raining:
+    print("Stay inside!");
+
+if not raining:
+        print("Go outside!");
+else:
+    print("Stay inside!");
