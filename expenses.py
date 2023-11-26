b2bcc280126832bae61874aa9d7510d03d7e0b9f
Tevrese Young <tevrese12@gmail.comgit config --global user.email tevrese12@gmail.comgit config --global user.email tevrese12@gmail.com>
2023-11-25 21:56:04 -0600

    added user input and loop

--- expenses.py
+++ expenses.py
@@ -1,4 +1,12 @@
-expenses = [10.50, 8, 5, 15, 20, 5, 3];
+total = 0;
+
+expenses = [];
+num_expenses = int(input("Enter # of expenses:"))
+
+for i in range(num_expenses):
+    expenses.append(float(input("Enter an expense:")))
+    
+
 
 total = sum(expenses)
 
